a1770a79410198eb5c8354154a6e4417bb12cfa0
skoenig3 <sethkoenig@gmail.com>
2017-02-03 17:19:53 -0800

    addded analysis for testing contextual differences

--- Reward_analysis.m
+++ Reward_analysis.m
@@ -18,7 +18,7 @@ function Reward_analysis(data_dir,figure_dir,session_data)
 figure_dir = [figure_dir 'Reward analysis\'];
 task = 'ListSQ';
 min_blks = 2;
-twin = 150;% how much time to take prior to ITT start and end
+twin = 200;% how much time to take prior to ITT start and end
 % Delay in HPC to visual stimuli around 150-200 ms so probably want at least 2x this
 reward_pulse_dur = 115;%ms for each pulse sound, audio file confirms sound last ~115 ms
 IRI = 166;%ms inter-reward-interval
@@ -28,7 +28,7 @@ reward_dur = 945; %116 ms per pulse *6;
 trial_start_code = 15; %ITI code
 imgon_code = 23;
 reward_code = 3;
-smval = 60 ;%gaussian 1/2 width for smoothing, 30 ms std
+smval = 60;%gaussian 1/2 width for smoothing, 30 ms std
 numshuffs = 1000;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -51,7 +51,8 @@ if num_units == 0
 end
 num_trials = length(cfg.trl);
 
-valid_trials = determine_valid_trials(task_file,valid_trials,cfg,num_units,min_blks);
+%determine which trials units are stable for
+valid_trials = determine_valid_trials(task_file,valid_trials,cfg,num_units,min_blks,task);
 if all(isnan(valid_trials(1,:)))
     disp([task_file(1:8) ': no valid units could be found. Exiting function...'])
     return %since no valid units skip analysis
@@ -65,7 +66,7 @@ disp('Aligning spike times to trial events')
 
 %get important task specific information
 [itmlist,sequence_items,~] = read_ListSQ_itm_and_cnd_files(item_file,cnd_file);
-[~,novel_vs_repeat,img_cnd] = get_image_numbers(cfg,itmlist,sequence_items,imgon_code);
+[which_img,novel_vs_repeat,img_cnd] = get_image_numbers(cfg,itmlist,sequence_items,imgon_code);
 
 %preallocate space and parallel structure of cfg
 time_locked_firing = cell(1,num_units);
@@ -89,9 +90,9 @@ for t = 1:num_trials
         reward_start = rewards(1);
         reward_end = rewards(end);
         
-%         if trial_end-reward_end < twin
-%             disp('what?')
-%         end
+        %         if trial_end-reward_end < twin
+        %             disp('what?')
+        %         end
         
         for unit = 1:num_units
             if t >= valid_trials(1,unit) && t <= valid_trials(2,unit) %only put data in for valid trials
@@ -159,47 +160,110 @@ for unit = 1:num_units
         temporal_info.temporalstability_prctile(2,unit) = 100*sum(...
             observed_info_rate.temporalstability(2) > ...
             shuffled_info_rate.temporalstability(2,:))/numshuffs;
-       
+        
     end
 end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%---Contextual Differences between Sequence 1 and Sequence 2---%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+reward_sig_times = cell(1,num_units); %difference between novel and repeated 
+for unit = 1:num_units
+    %only run on stable, peaky units
+    if (temporal_info.temporalstability_prctile(1,unit) > 95) && (temporal_info.rate_prctile(unit) > 95)
+         [~,all_firing_rate_curves] = nandens(time_locked_firing{unit},smval,'gauss',Fs,'nanflt'); %trial-by-trial smoothed
+        
+        all_curves = NaN(10*numshuffs,reward_dur+2*twin);
+        seq = trial_type{unit};
+        parfor shuff = 1:(10*numshuffs) %~10000
+            ind = randperm(length(seq));
+            shuffled_seq = seq(ind);
+            shuff_seq1 = nanmean(all_firing_rate_curves(shuffled_seq == 1,:));
+            shuff_seq2 = nanmean(all_firing_rate_curves(shuffled_seq == 2,:));
+            all_curves(shuff,:) = shuff_seq1-shuff_seq2;
+        end
+        
+        %observed curves
+        seq1_curve =nanmean(all_firing_rate_curves(seq == 1,:));
+        seq2_curve =nanmean(all_firing_rate_curves(seq == 2,:));
+        observed_diff = seq1_curve-seq2_curve;
+        [~,reward_sig_times{unit}] = cluster_level_statistic(observed_diff,all_curves,2,smval); %multiple comparision corrected significant indeces
+    end 
+end  
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%---Plot and Save Figures of Results---%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 t = -twin+1:reward_dur+twin;
 unit_names = unit_stats(1,:);
 for unit = 1:num_units
     if ~isempty(time_locked_firing{unit})
         
+        yls = NaN(2,2);
         
         figure
         
         %firing rate locked to Reward start
         subplot(2,2,1)
         dofill(t,time_locked_firing{unit},'k',1,smval);
-        hold on
-        dofill(t,time_locked_firing{unit}(trial_type{unit} == 1,:),'r',1,smval);
-        dofill(t,time_locked_firing{unit}(trial_type{unit} == 2,:),'b',1,smval);
         ylims = ylim;
         if ylims(1) < 0
             ylims(1) = 0;
             ylim([0 ylims(2)])
         end
-        for i = 1:6
-            plot([(i-1)*IRI (i-1)*IRI],[ylims(1) ylims(2)],'k--')
-        end
+        yls(1,:) = ylims;
         xlabel('Time from  Reward start (ms)')
         ylabel('Firing Rate (Hz)')
-        title_str = ['n = ' num2str(size(time_locked_firing{unit},1))];
+        title_str = ['All trials, n = ' num2str(size(time_locked_firing{unit},1))];
         if temporal_info.rate_prctile(unit) > 95 || temporal_info.temporalstability_prctile(unit) > 95
             title_str = [title_str ', bit = ' num2str(temporal_info.rate_prctile(unit),3) ...
                 '%, \rho_{1/2} = ' num2str(temporal_info.temporalstability(1,unit),2) ...
                 ' ' num2str( temporal_info.temporalstability_prctile(1,unit),3) '%'];
         end
         xlim([-twin twin+reward_dur])
-        title(title_str)
-        legend('All','Seq #1','Seq #2')
+        title(title_str)        
+        subplot(2,2,2)
+        dofill(t,time_locked_firing{unit}(trial_type{unit} == 1,:),'r',1,smval);
+        hold on
+        dofill(t,time_locked_firing{unit}(trial_type{unit} == 2,:),'b',1,smval);
+         ylims = ylim;
+        if ylims(1) < 0
+            ylims(1) = 0;
+            ylim([0 ylims(2)])
+        end
+        yls(2,:) = ylims;
+        plot([0 0],[ylims(1) ylims(2)],'k--')
+        hold off
+        legend('Seq#1','Seq#2')
+        
+        ymin = min(yls(:,1));
+        ymax = max(yls(:,2));
+        subplot(2,2,1)
+        ylim([ymin ymax]);
+        hold on
+        for i = 1:6
+            plot([(i-1)*IRI (i-1)*IRI],[ymin ymax],'k--')
+        end
+        hold off
+        
+        subplot(2,2,2)
+        ylim([ymin ymax]);
+        if ~isempty(reward_sig_times{unit})
+            gaps = findgaps(find(reward_sig_times{unit}));
+            hold on
+            if ~isempty(gaps)
+                for g = 1:size(gaps,1)
+                    gp = gaps(g,:);
+                    gp(gp == 0) = [];
+                    h = fill([min(gp) max(gp) max(gp) min(gp) min(gp)]-twin,...
+                        [ymin ymin ymax ymax ymin],'k');
+                    uistack(h,'down')
+                    set(h,'facealpha',.25,'EdgeColor','None')
+                end
+            end
+            hold off
+        end
         
         %raster plot locked to Reward start
         subplot(2,2,3)
@@ -215,7 +279,7 @@ for unit = 1:num_units
         end
         ylabel('Trial #')
         xlabel('Time from  Reward start (ms)')
-
+        
         
         %---Plot Rasters for Sequence 1 and Sequence 2---%
         seq1 = time_locked_firing{unit}(trial_type{unit} == 1,:);
@@ -251,6 +315,6 @@ end
 %%%---Finally save all the data---%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 save([data_dir task_file(1:8) '-ListSQ-Reward_results.mat'],'time_locked_firing',...
-    'trial_type','smval','temporal_info','unit_names','twin','reward_dur');
+    'trial_type','smval','temporal_info','unit_names','twin','reward_dur','reward_sig_times');
 disp(['Reward Locked Data Analyis for ' task_file(1:8) ' saved']);
 end
