849e7a2fd9db2cd7cd9b7172f7fa56f5e5c2605b
skoenig3 <sethkoenig@gmail.com>
2017-03-28 14:41:37 -0700

    added, now commented out code, to make population level time cell plots

--- Population_Saccade_Direction.m
+++ Population_Saccade_Direction.m
@@ -21,6 +21,13 @@ Fs = 1000; %Hz sampling frequency
 imageX = 800; %horizontal image size
 imageY = 600; %vertical image size
 
+bin_deg = 1; %number of degrees per bin
+bin_deg2 = 45; %initial degree bins to determine time of peak direction modualtion
+smval_deg = 18; %9 degrees std
+smval = 30; %smoothing in time
+
+
+
 %---Values All Fixations out2out and in2in---%
 all_mrls = []; %all observed MRLs (mean resultant vector length) ignoring out2in and in2out
 all_mrl_pctiles = []; %observed MRLs shuffled percentile ignoring out2in and in2out
@@ -33,6 +40,11 @@ all_mrl_out_pctiles = []; %observed MRLs shuffled percentile ignoring out2in and
 spatialness = []; %1 for place cell, 0 for non place cell
 all_unit_names = {};
 all_monkeys = []; %1s and 2s for monkeys
+
+prefered_firing_rate_curves = [];
+anti_prefered_firing_rate_curves = [];
+ratio_prefered = [];
+all_windows  = [];
 
 figure_dir = {};
 all_dirs = []; %saccade directions for significant units
@@ -96,7 +108,7 @@ for monkey = 2:-1:1
         if exist([data_dir task_file(1:8) '-Saccade_Direction_Analysis.mat'],'file') %want to remove later
             load([data_dir task_file(1:8) '-Saccade_Direction_Analysis.mat'])
             load([data_dir task_file(1:end-11) '-spatial_analysis_results.mat'],'spatial_info')
-            load([data_dir  task_file(1:8) '-Place_Cell_Analysis.mat'],'saccade_direction');
+            load([data_dir  task_file(1:8) '-Place_Cell_Analysis.mat']);
         else
             if num_units ~= 0
                 error('Where is this file')
@@ -113,7 +125,7 @@ for monkey = 2:-1:1
                 all_mrl_pctiles = [all_mrl_pctiles mrls.all_fixations_shuffled_prctile(unit)];%observed MRLs shuffled percentile ignoring out2in and in2out
                 
                 all_dirs = [all_dirs saccade_direction{unit}];
-
+                
                 %---Values All Fixations out2out only---%
                 all_mlrs_out = [all_mlrs_out mrls.out2out(unit)]; %observed MRLs for out2out fixations only
                 all_mrl_out_pctiles = [all_mrl_out_pctiles mrls.out2out_shuffled_prctile(unit)]; %observed MRLs shuffled percentile ignoring out2in and in2out
@@ -126,6 +138,85 @@ for monkey = 2:-1:1
                 else
                     spatialness = [spatialness 0]; %non place cell
                 end
+                
+                if  mrls.all_fixations_shuffled_prctile(unit) > 95
+                    
+                    %---Get Firing Rate Curves for Prefered vs AntiPreferredDirections---%
+                    %unfortunately not enough was saved
+                    degrees = [0:bin_deg:360]-180;
+                    egrees = degrees(2:end);
+                    degrees = degrees*pi/180;
+                    % degrees = [degrees degrees(1)];
+                    
+                    fix_aligned = list_fixation_locked_firing{unit}; %fixation aligned firing rate
+                    sac_dirs = saccade_direction{unit}; %saccade directions organized the same way as fix_algined
+                    fr = nandens(fix_aligned,smval,'gauss',1000,'nanflt'); %firing rate curve aligned to fixations
+                    
+                    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                    %%%---Method 2: Determine time window of interest based on Time of Peak Firing Rate Modulation---%%%
+                    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                    %prefered since we currently assume most units show a preference for
+                    %certain stimulus features at a particular time relative to eye
+                    %movements. Method simply finds the maximum firing rate deviation from
+                    %the mean when we bin the firing rate curves into arbitrary directions.
+                    %Note: the exact bins are not important for this method just to remove
+                    %the anti-prefered direction since this seems to often cause inhbition.
+                    degrees2 = [0:bin_deg2:360]-180;
+                    curves = NaN(length(degrees2),twin1+twin2);
+                    for bin = 2:length(degrees2)
+                        these_directions = (sac_dirs < degrees2(bin) & sac_dirs >= degrees2(bin-1));
+                        curves(bin,:) = nandens(fix_aligned(these_directions,:),30,'gauss',Fs);
+                    end
+                    mean_of_curves = nanmean(fr);
+                    crude_direction_curves = curves; %save for later
+                    curves = abs(curves-mean_of_curves); %negatvie and positive work
+                    %don't include more than 100 ms before fixation and 200 ms after, have
+                    %yet to see a good example with prominent tuning outside this anyway
+                    time_of_max_modulation = find(curves(:,100:400) == max(max(curves(:,100:400))));
+                    [~,time_of_max_modulation] = ind2sub(size(curves(:,100:400)),time_of_max_modulation); %got to convert back into time index
+                    time_of_max_modulation = time_of_max_modulation+100; %since ingored the first 100 ms before
+                    window = time_of_max_modulation-((window_width/2)-1):time_of_max_modulation+window_width/2; %take window around peak
+                    
+                    if (spatial_info.shuffled_rate_prctile(unit) > 95) || (spatial_info.spatialstability_halves_prctile(1,unit) > 95)
+                        fix_aligned = fix_aligned(in_out{unit} == 2 | in_out{unit} == 4,:); %fixations in2in or out2out
+                        sac_dirs = sac_dirs(in_out{unit} == 2 | in_out{unit} == 4); %directions for fixations in2in or out2out
+                    end
+                    
+                    all_windows = [all_windows {window}];
+                    
+                    bin_deg = 12;
+                    [mean_binned_firing_rate,degrees,mrl] = bin_directional_firing(bin_deg,fix_aligned,window,sac_dirs);
+                    binned_firing_rate_curves{1,unit} = mean_binned_firing_rate; %binned firing rates
+                    [prefered_dirs,anti_prefered_dirs,smoothed_direction_curve] = ...
+                        select_prefred_indeces(binned_firing_rate_curves{1,unit},degrees,sac_dirs,2);
+                    
+                    antipref = nandens3(fix_aligned(anti_prefered_dirs,:),smval,1000);
+                    pref = nandens3(fix_aligned(prefered_dirs,:),smval,1000);
+                    a = antipref;
+                    a(a < 0.1) = mean(a);
+                    a(a == 0) = 1;
+                    ratio_prefered = [ratio_prefered; pref./a];
+                    
+%                     if max(pref./a) > 10
+%                        disp('now') 
+%                     end
+                    
+                    if any(isnan(ratio_prefered(end,:)))
+                        disp('now')
+                    elseif sum(isinf(ratio_prefered(:))) > 0
+                        disp('now')
+                    end
+                    
+                    pref = pref-mean(pref);
+                    pref = pref/max(abs(pref));
+                    antipref = antipref-mean(antipref);
+                    antipref = antipref/max(abs(antipref));
+                    
+                    prefered_firing_rate_curves = [prefered_firing_rate_curves; pref];
+                    anti_prefered_firing_rate_curves = [anti_prefered_firing_rate_curves; antipref];
+                end
+                
+                %%
             end
         end
     end
@@ -169,3 +260,47 @@ bf = [binned_dirs(end-(3*smval_deg):end) binned_dirs binned_dirs(1:3*smval_deg)]
 binned_dirs = nandens(bf,smval_deg,'gauss',1); %smooth to display firing rate by saccade direction
 binned_dirs = binned_dirs(3*smval_deg+2:end-3*smval_deg);%remove buffers
 polarplot(degrees,[binned_dirs(end) binned_dirs])
+
+%%
+tm = -twin1:twin2-1;
+figure
+subplot(2,2,1)
+hold on
+plot(tm,nanmean(anti_prefered_firing_rate_curves),'r')
+plot(tm,nanmean(prefered_firing_rate_curves),'g')
+plot([tm(1) tm(end)],[0 0],'k')
+plot([0 0],[-1 1],'k--')
+hold off
+xlabel('Time From Fixation Start (ms)')
+ylabel('Normalized Firing Rate')
+title('Population Average Mormalzed Firing Rates')
+legend('Anti-prefered','Prefered')
+
+subplot(2,2,2)
+[~,mxi] = max(prefered_firing_rate_curves');
+[~,si] = sort(mxi);
+imagesc(tm,1:size(prefered_firing_rate_curves,1),prefered_firing_rate_curves(si,:))
+xlabel('Time From Fixation Start (ms)')
+ylabel('Neuron #')
+colormap('jet')
+title('Time Cell Plot')
+%%
+subplot(2,2,3)
+plot(tm,nandens3(ratio_prefered,20,1))
+xlabel('Time From Fixation Start (ms)')
+ylabel('Ratio')
+title('Average Ratio of Prefered/Anti-Prefered')
+box off
+
+%%
+window_conc = zeros(1,twin1+twin2);
+for w = 1:length(all_windows);
+    window_conc(all_windows{w}) = window_conc(all_windows{w})+1;
+end
+
+subplot(2,2,4)
+plot(tm,100*window_conc/length(all_windows))
+xlabel('Time From Fixation Start (ms)')
+ylabel('% of Units')
+title('Distribution of 100 ms Window with Greatest Direction Modulation')
+box off
